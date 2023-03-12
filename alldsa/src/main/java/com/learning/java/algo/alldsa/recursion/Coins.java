cc152e40d7a01bf33e04d7897b60668759b65639
Vivek Tiwari <reachtovivek.t@gmail.com>
2023-03-12 07:38:02 +0530

    coins

--- alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/Coins.java
+++ alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/Coins.java
@@ -16,19 +16,54 @@ public class Coins {
         return calculateChange(n, coins, 0);
     }
 
-    private static int calculateChange(int amount, int[] coins, int position) {
+    //Recursive
+    public static int calculateChange(int amount, int[] coins, int position){
 
-        if (position >= coins.length - 1) {
+        if(position >= coins.length-1){
             return 1;
         }
-
         int coin = coins[position];
         int count = 0;
-        for (int i = 0; i * coin <= amount; i++) {
-            int remaining = amount - i * coin;
-            count += calculateChange(remaining, coins, position + 1);
+        for(int i = 0 ; i*coin<amount ; i++){
+            int remaningAmount = amount - i*coin;
+            count += calculateChange(remaningAmount, coins , position+1);
         }
-
         return count;
     }
+
+    //Memoization
+    public static int calculateChangeMemo(int n){
+        if(n <= 0){
+            return -1;
+        }
+        int[] coins = {25, 10, 5, 1};
+        int[][] cache = new int[n+1][coins.length];
+        return calculateChangeMemo(n, coins, 0, cache);
+    }
+
+    public static int calculateChangeMemo(int amount, int[] coins, int position, int[][] cache){
+
+        if(cache[amount][position] > 0){
+            return cache[amount][position];
+        }
+        if(position >= coins.length-1){
+            return 1;
+        }
+        int coin = coins[position];
+        int count = 0;
+        for(int i = 0 ; i*coin<amount ; i++){
+            int remaningAmount = amount - i*coin;
+            count += calculateChangeMemo(remaningAmount, coins , position+1,cache);
+        }
+        cache[amount][position] = count;
+        return count;
+    }
+
+    public static void main(String[] args){
+        System.out.println(Coins.calculateChange(25));
+
+        System.out.println(Coins.calculateChangeMemo(25));
+
+    }
+
 }
