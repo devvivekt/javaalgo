f19b0929a22bc18da1ecb9672dcd0665785a1c32
Vivek Tiwari <reachtovivek.t@gmail.com>
2023-03-14 23:26:16 +0530

    duppermutation

--- alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/StringPermutation.java
+++ alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/StringPermutation.java
@@ -1,5 +1,9 @@
 package com.learning.java.algo.alldsa.recursion;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringPermutation {
 
     public static void simplePermutation(String str){
@@ -21,8 +25,32 @@ public class StringPermutation {
         }
     }
 
+    public static Set<String> dupPermutation(String str){
+        if(str == null || str.isBlank()){
+            return Collections.emptySet();
+        }
+        return dupPermutation("", str);
+    }
+
+    public static Set<String> dupPermutation(String prefix, String str){
+        Set<String> permutations = new HashSet<>();
+        int n = str.length();
+        if(n == 0){
+            permutations.add(prefix);
+        }else{
+            for(int i=0; i< str.length(); i++){
+                permutations.addAll(dupPermutation(prefix+ str.charAt(i)
+                        , str.substring(i+1,n) + str.substring(0,i)));
+            }
+        }
+        return permutations;
+    }
+
+
     public static void main(String[] args){
         String str = "two";
         StringPermutation.simplePermutation(str);
+        str = "hellohowru"
+        StringPermutation.dupPermutation(str);
     }
 }
