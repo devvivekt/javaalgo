f8183f8fdfc09dbe3483be6899ef010442c4361f
Vivek Tiwari <reachtovivek.t@gmail.com>
2023-03-15 22:09:33 +0530

    knight tour

--- alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/KnightTour.java
+++ alldsa/src/main/java/com/learning/java/algo/alldsa/recursion/KnightTour.java
@@ -14,15 +14,54 @@ public class KnightTour {
     public static final int ROW[] = {1,-1,-2,-2,-1, 1, 2, 2};
     public static final int COL[] = {2, 2, 1,-1,-2,-2,-1, 1};
 
-    public static void knightTour(int r, int c, int cell, int visited[][]){
+    public void knightTour(int r, int c, int cell, int visited[][]){
 
         if(r<0 || c< 0 || cell < 0){
-            throw new IllegalArgumentException("row orc col or cell cant be negative");
+            throw new IllegalArgumentException("row or col or cell cant be negative");
         }
 
         if(visited == null){
             throw new IllegalArgumentException("the visited[][] cant be null");
         }
+
+        visited[r][c] = cell;
+
+        if(cell >= n*n){
+            print(visited);
+            visited[r][c] = 0;
+            return;
+        }
+
+        for(int i = 0 ; i < ROW.length-1; i++){
+            int newR = r + ROW[i];
+            int newC = c + COL[i];
+            if(isValid(newR, newC) && visited[newR][newC] ==0){
+                knightTour(newR, newC, cell+1 , visited);
+            }
+        }
+        //backtrack from current cell and remove it from current path
+        visited[r][c] = 0;
+    }
+
+    private boolean isValid(int r, int c){
+        return !(r<0 || c< 0 || r>= n || c >=n);
+    }
+
+    private void print(int[][] visited){
+        for(int i=0; i< n; i++){
+            for(int j = 0; j< n ; j++){
+                System.out.format("%3s", visited[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int n = 8;
+        KnightTour tour = new KnightTour(n);
+        int[][] visited = new int[n][n];
+        tour.knightTour(1,1,1, visited);
     }
 
 }
